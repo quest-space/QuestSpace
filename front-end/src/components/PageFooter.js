c264c49c4771b2395328c336708f67c426a8a848
Talal <talaltouseef@gmail.com>
2021-04-07 21:10:22 +0500

    Created the PageFooter component

---
+++ front-end/src/components/PageFooter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+// import linkedin.png from "../logo/linked.png"
+
+const PageFooter = (props) => {
+  return (
+    <MDBFooter  >
+      <MDBContainer fluid >
+        <MDBRow >
+          <MDBCol md="4" className="footerLeft">
+                QuestSpace
+          </MDBCol>
+          <MDBCol md="4"  className="footerMid">
+            <img src="/logos/linkedin.png" style={{paddingRight:"10px"}}></img>
+            <img src="/logos/github.png" style={{paddingLeft:"10px"}}></img>
+          </MDBCol>
+          <MDBCol  className="footerRight">
+              Come and join our space!
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </MDBFooter>
+  );
+}
+
+export default PageFooter;
