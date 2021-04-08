8361f8788dfb982327f0f1f51237af4352088580
AnnaMazhar <ainamazhar998@gmail.com>
2021-04-09 01:55:32 +0500

    Adding Homepage

--- front-end/front-end-routes.js
+++ front-end/front-end-routes.js
@@ -6,7 +6,7 @@ router = express.Router();
 router.use("/",express.static("front-end/build"));
 router.use("/signin",express.static("front-end/build"));
 router.use("/signup",express.static("front-end/build"));
-router.use("/card",express.static("front-end/build"));
+router.use("/homepage",express.static("front-end/build"));
 
 
 module.exports = router;
