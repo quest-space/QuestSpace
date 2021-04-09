f9ccb1764f498867233b2414fbb641a1df0d0562
Talal <talaltouseef@gmail.com>
2021-04-10 02:10:50 +0500

    Working on SignUp

--- front-end/src/components/CardLeft.js
+++ front-end/src/components/CardLeft.js
@@ -1,7 +1,21 @@
 import React from "react"
 
 const CardLeft = () => {
-    const 
+    return (
+        <React.Fragment>
+            <div className="imgSpace">
+                <img src="/img/signup/1.png" >
+                </img>
+            </div>
+
+            <svg className="whiteRect">
+            </svg>
+
+            <svg className="backRect">
+                <rect />
+            </svg>
+        </React.Fragment>
+    )
 }
 
 export default CardLeft
--- front-end/src/components/SignUpPage.js
+++ front-end/src/components/SignUpPage.js
@@ -1,9 +1,10 @@
 import React from "react"
 import PageFooter from "./PageFooter"
 import { Container, Row, Col } from "react-bootstrap"
-import Image from 'react-bootstrap/Image'
 import "../css/SignUp.css"
 import CreateAccount from "./CreateAccount"
+import CardLeft from "./CardLeft"
+import CardRight from "./CardRight"
 
 const SignUpPage = () => {
     return (
@@ -12,31 +13,17 @@ const SignUpPage = () => {
             <Container className="d-none d-md-block" style={{ border: "solid" }}>
                 <Row >
                     <Col md="6" className="leftCol" >
-                        <div className="imgSpace">
-                            <img src="/img/signup/1.png" >
-                            </img>
-                        </div>
-
-                        <svg className="whiteRect">
-                        </svg>
-
-                        <svg className="backRect">
-                            <rect />
-                        </svg>
+                        <CardLeft />
                     </Col>
                     <Col md="6" className="rightCol" style={{ border: "none" }}>
-                        <div className="backForm">
-                            <CreateAccount />
-                        </div>
+                        <CardRight child={<CreateAccount />} />
                     </Col>
                 </Row>
             </Container>
-            <Container className="d-md-none" style={{ border: "solid", paddingTop: "110px" } }>
+            <Container className="d-md-none" style={{ border: "solid", paddingTop: "110px" }}>
                 <Row>
                     <Col md="6" className="rightCol" style={{ border: "none", margin: "auto", width: "100%" }}>
-                        <div className="backForm" style={{ position: "relative", margin: "auto" }}>
-                            <CreateAccount />
-                        </div>
+                        <CardRight child={<CreateAccount />} style={{ position: "relative", margin: "auto" }} />
                     </Col>
                 </Row>
             </Container>
