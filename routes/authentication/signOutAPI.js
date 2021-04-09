f2c366fd50a1b7b48cab8f91de2f6b210e74f4a5
Talha Waheed <talhawaheed57@gmail.com>
2021-04-09 12:18:58 +0500

    Added signout API

--- routes/authentication/signOutAPI.js
+++ routes/authentication/signOutAPI.js
@@ -1,9 +1,14 @@
 const { Router } = require (`express`);
 const router = Router();
 
+const { sendRes } = require(`../helpers/sendRes`);
+
+const OK_STATUS_CODE = 200;
+
 // log out user
 router.post(`/`, async (req, res) => {
   res.cookie(`qsUser`, ``, { maxAge: 1 });
+  sendRes(res, OK_STATUS_CODE, {});
 });
 
 module.exports = router;
