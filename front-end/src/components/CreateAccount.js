48fee6774711f3b929f8e87803b5e554990b710c
Talal <talaltouseef@gmail.com>
2021-04-10 01:04:03 +0500

    Working on Signup

---
+++ front-end/src/components/CreateAccount.js
@@ -0,0 +1,77 @@
+import React from "react"
+import "../css/SignUp.css"
+import "bootstrap"
+
+
+const CreateAccount = () => {
+
+    const [user, setUser] = React.useState(true)
+
+    const switchUser = () => {
+        setUser(!user)
+    }
+
+    return (
+        <div className="createAccount">
+            <div className="header">
+                <div className="title">
+                    Create an Account
+                </div>
+
+                <div className="titleCaption">
+                    Already have one?&nbsp;
+                    <a className="titleCaption" style={{ textDecorationLine: "underline" }} href="/signin">
+                        Sign In
+                    </a>
+                </div>
+
+            </div>
+
+            <nav className="nav nav-pills nav-justified">
+                <button className={`btn${user}`} onClick={switchUser} style={{ borderRadius: "30px 0px 0px 30px" }}>
+                    Participant
+                </button>
+                <button className={`btn${!user}`} onClick={switchUser} style={{ borderRadius: "0px 30px 30px 0px" }}>
+                    Host
+                </button>
+            </nav>
+
+            <form className="form">
+                <div className="formRow">
+                    <div className="formCol">
+                        <label className="formLabel">Username</label>
+                    </div>
+                </div>
+                <div className="formRow">
+                    <div className="formCol">
+                        <input type="text" className="input" placeholder="Enter here" />
+                    </div>
+                </div>
+
+                <div className="formRow">
+                    <div className="formCol">
+                        <label className="formLabel">Password</label>
+                    </div>
+                </div>
+                <div className="formRow">
+                    <div className="formCol">
+                        <input type="password" className="input" placeholder="Enter here" />
+                    </div>
+                </div>
+            </form>
+
+            <div style={{paddingTop: "20px", textAlign: "center"}}>
+                <button className="btnNext">
+                    Next
+                </button>
+            </div>
+                
+
+            <i className="fa fa-angle-right" aria-hidden="true"></i>
+            
+
+        </div>
+    )
+}
+
+export default CreateAccount
--- front-end/src/components/SignUpPage.js
+++ front-end/src/components/SignUpPage.js
@@ -1,22 +1,42 @@
 import React from "react"
 import PageFooter from "./PageFooter"
 import { Container, Row, Col } from "react-bootstrap"
+import Image from 'react-bootstrap/Image'
 import "../css/SignUp.css"
+import CreateAccount from "./CreateAccount"
 
 const SignUpPage = () => {
     return (
         <React.Fragment>
             {/* Navbar from Hareem */}
-            <Container style={{border: "solid"}}>
-                <Row style={{ padding: "0px" }}>
-                    <Col md="6" className="d-none d-md-block" style={{border: "solid", textAlign: "Right"}}>
-                        <img src="/img/signup/1.png" width="415" height="600"  >
+            <Container className="d-none d-md-block" style={{ border: "solid" }}>
+                <Row >
+                    <Col md="6" className="leftCol" >
+                        <div className="imgSpace">
+                            <img src="/img/signup/1.png" >
+                            </img>
+                        </div>
 
-                        </img>
-                        
+                        <svg className="whiteRect">
+                        </svg>
+
+                        <svg className="backRect">
+                            <rect />
+                        </svg>
                     </Col>
-                    <Col md="6" style={{border: "solid"}}>
-                        world
+                    <Col md="6" className="rightCol" style={{ border: "none" }}>
+                        <div className="backForm">
+                            <CreateAccount />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="d-md-none" style={{ border: "solid", paddingTop: "110px" } }>
+                <Row>
+                    <Col md="6" className="rightCol" style={{ border: "none", margin: "auto", width: "100%" }}>
+                        <div className="backForm" style={{ position: "relative", margin: "auto" }}>
+                            <CreateAccount />
+                        </div>
                     </Col>
                 </Row>
             </Container>
