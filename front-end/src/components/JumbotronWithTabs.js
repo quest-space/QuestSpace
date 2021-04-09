e417f4cae3b68f8687487140f021d4320e7e2bed
AnnaMazhar <ainamazhar998@gmail.com>
2021-04-09 12:45:51 +0500

    Uniformed CSS across webpages

--- front-end/src/components/JumbotronWithTabs.js
+++ front-end/src/components/JumbotronWithTabs.js
@@ -29,16 +29,16 @@ const JumbotronWithTabs = () => {
 
   return(
     <div>
-      <div class="jumbotron jumbotron-fluid" style={{marginBottom:'0em' ,background: "linear-gradient(209.34deg, rgba(71, 111, 143, 0) 17.99%, #335875 177.27%), #1F394E"}}>
-        <div class="container">
-          <h1 class="display-4" style={{fontWeight:"500", fontSize:"42px", color:"#ffffff", marginLeft:'0.6em'}}>QuestSpace</h1>
+      <div id='fancy' className="jumbotron jumbotron-fluid" style={{marginBottom:'0em' ,background: "linear-gradient(209.34deg, rgba(71, 111, 143, 0) 17.99%, #335875 177.27%), #1F394E"}}>
+        <div className="container">
+          <h1 className="display-4" style={{fontWeight:"500", fontSize:"42px", color:"#ffffff", marginLeft:'0.6em'}}>QuestSpace</h1>
         </div>
       </div>
       {/* TABS */}
-      <div class="col-md-12" style={{margin:'0em', padding:'0em'}}>
+      <div className="col-md-12" style={{margin:'0em', padding:'0em'}}>
         <ul id="top" style={{margin:'0em', padding:'0em'}}>
           <li><button className="parent" onClick = {()=>setBar("home")} style={{borderBottom: bar1, width: '22rem'}}>Home</button></li>
-          <li className="subnav"><button className="parent" onClick = {()=>setBar("my")} style={{borderBottom: bar2, width: '22rem'}} >My Quests<i class="fa fa-caret-down"></i></button>
+          <li className="subnav"><button className="parent" onClick = {()=>setBar("my")} style={{borderBottom: bar2, width: '22rem'}} >My Quests<i className="fa fa-caret-down"></i></button>
           <ul id="subnav-list" style={{margin:'0em', padding:'0em'}}>
             <li><button className="kid" >All</button></li>
             <li><button className="kid" >Live</button></li>
