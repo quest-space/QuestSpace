41efe7c1f5d00ef136c2a8aa6819b648444cd709
Talal <talaltouseef@gmail.com>
2021-04-11 03:38:16 +0500

    Worked on Rapid Fire

--- front-end/src/components/Round.js
+++ front-end/src/components/Round.js
@@ -2,18 +2,47 @@ import React from "react"
 import MainNavbar from "./MainNavbar"
 import Header from "./Header"
 import Question from "./Question"
+import PageFooter from "./PageFooter"
 import { Container, Row, Col } from "react-bootstrap"
 
 
 const Round = () => {
+    const options = ["Pubg", "battlefield", "fortnite", "call of duty"]
+
     return (
         <React.Fragment>
             <MainNavbar />
             <Header />
 
-            <Container className="questionContainer" style={{ width: "60%" }}>
-                <Question />
+            <Container className="questionContainer d-none d-md-block" style={{ width: "60%" }}>
+
+                <div className="timer">
+
+                    <i className="fas fa-stopwatch" style={{ color: "#575757" }}></i>
+
+                </div>
+
+                <Question questionNumber="1"
+                    question="Which popular game has released games with title World at War and Black Ops?"
+                    options={options} />
+
             </Container>
+
+            <Container className="questionContainer d-md-none" style={{ width: "100%" }}>
+
+                <div className="timer">
+
+                    <i className="fas fa-stopwatch" style={{ color: "#575757" }}></i>
+
+                </div>
+
+                <Question questionNumber="1"
+                    question="Which popular game has released games with title World at War and Black Ops?"
+                    options={options} />
+
+            </Container>
+
+            <PageFooter />
         </React.Fragment>
     )
 }
