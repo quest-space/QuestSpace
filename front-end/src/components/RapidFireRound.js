fc0e7e226dfafd61fc6d8a849f0f69b472eec5b6
Talal <talaltouseef@gmail.com>
2021-04-11 00:35:20 +0500

    Added RapidFireRound

---
+++ front-end/src/components/RapidFireRound.js
@@ -0,0 +1,17 @@
+import React from "react"
+import MainNavbar from "./MainNavbar"
+import Header from "./Header"
+import HeadSubhead from "./HeadSubhead"
+import "../css/RapidFireRound.css"
+
+const RapidFireRound = () => {
+    return (
+        <React.Fragment>
+            <MainNavbar />
+            <Header />
+            hello
+        </React.Fragment>
+    )
+}
+
+export default RapidFireRound
