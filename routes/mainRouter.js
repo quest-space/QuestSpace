08d867889ec315fc5de144d408f019ce37df7e05
Talha Waheed <talhawaheed57@gmail.com>
2021-04-10 06:08:04 +0500

    Added verification for quest requestor

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,9 +2,9 @@ const { Router } = require (`express`);
 const router = Router();
 
 // routes API requests to different files
-// router.use(`/auth`, require(`./authentication/signUpInAPIs`));
+router.use(`/auth`, require(`./authentication/signUpInAPIs`));
 // Filters authenticated users and refreshes their authentication:
-// router.use(require(`./authentication/authenticateUser`));
+router.use(require(`./authentication/authenticateUser`));
 // An authenticated user is who
 // 1. routes at /api/participant or /api/host
 // 2. has a token in cookie
