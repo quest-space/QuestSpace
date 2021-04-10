908efa9aff47ca514a5010c45ee6e4b277ef4f3f
Talal <talaltouseef@gmail.com>
2021-04-11 03:38:16 +0500

    Worked on Rapid Fire

--- front-end/src/components/Question.js
+++ front-end/src/components/Question.js
@@ -1,11 +1,16 @@
 import React from "react"
 import "../css/Question.css"
+import QuestionOption from "./QuestionOption"
 
 const Question = (props) => {
 
     return (
         <div className="questionBox" >
-            Q{props.questionNumber})
+            Q{props.questionNumber}) {props.question}
+            {props.options.map((option,index) =>{
+                return <QuestionOption questionNumber={index+1} option={option} key={index} />
+            })}
+
         </div>
     )
 }
