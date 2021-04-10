82ab08c922db7ef622cb04251d0ac088ba5166bf
Hareem <hareemraza2000@gmail.com>
2021-04-10 19:14:58 +0500

    Added link to QuestDetails

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import SignInPage from "./components/SignInPage";
 import Homepage from "./components/Homepage";
 import Header from "./components/Header";
 import InitialNavbar from "./components/InitialNavbar";
+import QuestDetails from "./components/QuestDetails";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/signin" component={SignInPage} />
         <Route path="/homepage" component={Homepage} />
         <Route path="/navbar1" component={InitialNavbar} />
+        <Route path="/questdetails" component={QuestDetails} />
         <Route path="/header" component={Header} />
         <Route exact path="/" component={LandingPage} />
       </Switch>
