88d0a11d0d15487405dfd32c6898898fe15d19dd
Talal <talaltouseef@gmail.com>
2021-04-11 19:05:52 +0500

    Sign out implemented

--- front-end/src/components/HostHomepage.js
+++ front-end/src/components/HostHomepage.js
@@ -1,10 +1,19 @@
 import React from "react"
+import MainNavbar from "./MainNavbar"
+import PageFooter from "./PageFooter"
 
 const HostHomepage = () => {
     return (
-        <h1>
-            Host Homepage
-        </h1>
+        <React.Fragment>
+            <MainNavbar />
+
+            <h1>
+                Host Homepage
+            </h1>
+
+            <PageFooter />
+        </React.Fragment>
+
     )
 }
 
