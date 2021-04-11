4f06ff74d9a9081c5e724fcb5af82bc17d789462
Talal <talaltouseef@gmail.com>
2021-04-11 20:46:00 +0500

    Implemented BreadCrumbs

--- front-end/src/components/BreadCrumb.js
+++ front-end/src/components/BreadCrumb.js
@@ -1,9 +1,26 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const BreadCrumb = (props) => {
+    console.log("items are", props.items)
     return (
         <div className="col-md-12" style={{ marginLeft: "0em", padding: "0em" }}>
-            <div id="top">{props.text}</div>
+            <div id="top">{props.items.map((item, index) => {
+                if (index === 0) {
+                    console.log(item)
+                    return (<Link to={item.to} style={{ color: "white" }}>
+                        {item.text}
+                    </Link>
+                    )
+                }
+                else {
+                    return (
+                        <Link to={item.to} style={{ color: "white" }}>
+                            {` > ${item.text}`}
+                        </Link>
+                    )
+                }
+            })}</div>
         </div>
     )
 }
