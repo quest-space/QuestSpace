3939748e29fd0f7669d0798dceb36a441e475c14
Ubuntu <ubuntu@ip-172-31-2-80.ap-south-1.compute.internal>
2021-04-17 12:41:43 +0000

    added alternative test api paths that dont required cookies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require(`cookie-parser`);
+const cors = require(`cors`);
 
 // port to listen at
 const PORT = 4333;
@@ -14,13 +15,14 @@ const Rating = require(`./models/ratings`);
 // set up express app
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(cors({ origin: true, credentials: true }));
+//app.use((req, res, next) => {
+//  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
+//  res.header("Access-Control-Allow-Credentials", true);
+//  next();
+//});
 
 app.use(require('./front-end/front-end-routes'));
 
@@ -30,6 +32,9 @@ app.use(cookieParser()); // Parse Cookies
 
 // initialize routes
 app.use('/api', require('./routes/mainRouter'));
+
+// test routes
+app.use(`/apitest`, require(`./routes/testRouter`));
 
 // error handling (from routes) middleware
 app.use((err, req, res, next) => {
