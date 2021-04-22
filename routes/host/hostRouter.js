75a2b2c7693c5498b855d151d6d617863c103284
Hassaan Ahmad <hassaanahmad45@gmail.com>
2021-04-23 04:44:56 +0500

    Added host/participant View and Edit Profile

--- routes/host/hostRouter.js
+++ routes/host/hostRouter.js
@@ -2,6 +2,7 @@ const { Router } = require (`express`);
 const router = Router();
 
 router.use(`/signout`, require(`../authentication/signOutAPI`));
+router.use(`/profile`, require(`./host-profile`));
 router.use(`/leaderboard`, require(`./leaderboard`));
 router.use(`/search`, require(`../helpers/search`));
 router.use(`/create-update`, require(`./createQuest`));
