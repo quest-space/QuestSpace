1486fcffd91de39b34211b9b1ca982ffb249c6d0
Anna Mazhar <anna@noob3000.local>
2021-04-24 02:20:06 +0500

    Edit Round front-end implemented

--- front-end/src/components/HostRoundsList.js
+++ front-end/src/components/HostRoundsList.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { useParams } from "react-router-dom"
 
 const HostRoundsList = (props) => {
 
+    const { questID } = useParams()
+
+    const [displayForm, setDisplayForm] = React.useState(false)
+
+    const addQuest = async () => {
+    
+        const response = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/apitest/host/quest/${questID}/addround`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                username: 'HassaanAW',
+                password: 'hassaan123',
+            }),
+        })
+
+        const responseBody = await response.json()
+
+        if (responseBody.status !== 200) {
+            console.log(`Error fetching.`)
+        } else {
+            console.log(`Successful fetching.`)
+        }
+    
+    }
+
+    const deleteQuest = async () => {
+    
+        const response = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/apitest/host/quest/${questID}/deleteround`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                username: 'HassaanAW',
+                password: 'hassaan123',
+            }),
+        })
+
+        const responseBody = await response.json()
+
+        if (responseBody.status !== 200) {
+            console.log(`Error fetching.`)
+        } else {
+            console.log(`Successful fetching.`)
+        }
+    
+    }
 
     const left = {
         fontWeight: "normal",
@@ -28,9 +80,120 @@ const HostRoundsList = (props) => {
                 )
             })}
 
+            {
+                displayForm === true && <div className="myBox" style={{paddingRight:"0rem"}}>
+                    <button class="tick1"> <i class="fas fa-check"></i></button>
+                    <button class="cross1" onClick={()=> {setDisplayForm(false)}}> <i class="fas fa-times"></i></button>
+                    
+                    <tr>
+                    
+                    <p  style={{fontWeight: "600",fontSize: "22px", marginBottom:"0rem", display:"inline-block"}}>Add Round</p>
+                    <th style={{display:"block"}}>
+                    <p
+                        className="display-4"
+                        style={{
+                            paddingTop: "0.5rem",
+                            fontWeight: "400",
+                            fontSize: "18px",
+                            color: "#46B7A1",
+                            marginLeft: "0rem",
+                            wordWrap: "break-word",
+                        }}
+                        >
+                        Name
+                            <input
+                            type="text"
+                            className="inputdetail responsive"
+                            style={{fontSize:"18px",paddingTop: "0.5rem",display:"block"}}
+                            placeholder="Enter here"
+                            // onChange={(ev) => updateState(ev, setQuestName)}
+                            />
+                    </p>
+                    </th>
+
+                    </tr>
+                    <tr class="container responsive">
+                    <th>
+                        <p
+                            className="display-4"
+                            style={{
+                                paddingTop: "0.5rem",
+                                fontWeight: "400",
+                                fontSize: "18px",
+                                color: "#46B7A1",
+                                marginLeft: "0rem",
+                                wordWrap: "break-word",
+                            }}
+                            >
+                            Start
+                            <input
+                            type="text"
+                            className="inputdetail responsive"
+                            style={{fontSize:"18px",paddingTop: "0.5rem",display:"block"}}
+                            placeholder="Enter here"
+                            // onChange={(ev) => updateState(ev, setQuestName)}
+                            />
+        
+                        </p>
+                    </th>
+                    <th>
+                        <p
+                            className="display-4"
+                            style={{
+                                paddingTop: "0.5rem",
+                                paddingLeft:"3rem",
+                                fontWeight: "400",
+                                fontSize: "18px",
+                                color: "#46B7A1",
+                                marginLeft: "0rem",
+                                wordWrap: "break-word",
+                            }}
+                            >
+                            End
+                            <input
+                            type="text"
+                            className="inputdetail responsive"
+                            style={{fontSize:"18px",paddingTop: "0.5rem",display:"block"}}
+                            placeholder="Enter here"
+                            // onChange={(ev) => updateState(ev, setQuestName)}
+                            />
+        
+                        </p>
+                    </th>
+
+                    <th>
+                    <div class="form-group form-inline" style={{fontWeight: "600",fontSize: "22px", marginBottom:"0rem", display:"block"}}>
+                    <p
+                            className="display-4"
+                            style={{
+                                paddingTop: "0.5rem",
+                                paddingLeft:"3rem",
+                                marginBottom: "0rem",
+                                fontWeight: "400",
+                                fontSize: "18px",
+                                color: "#46B7A1",
+                                marginLeft: "0rem",
+                                wordWrap: "break-word",
+                            }}
+                            >
+                            Type
+                    </p>
+                    <select class="form-control" style={{marginLeft:"3rem"}} id="sel1">
+                        <option>Rapid Fire</option>
+                        <option>Quiz</option>
+                        <option>Submission based</option>
+                    </select>
+                    </div>
+                    </th>
+
+                    </tr>
+                </div>
+            }
+
             <div className="myBox dashedBox1">
-                <button style={{width: "100%", backgroundColor: "#FFFFFF", border: "none" }}><i class="fas fa-plus"></i> </button>
+                <button style={{width: "100%", backgroundColor: "#FFFFFF", border: "none" }} onClick={()=> {setDisplayForm(true)}}><i class="fas fa-plus"></i> </button>
             </div>
+
         </div>
     )
 
