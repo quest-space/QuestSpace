b585e5dae10dfb7e018bdc3437a393a2a3eabaf3
Talha Waheed <talhawaheed57@gmail.com>
2021-04-23 21:10:19 +0500

    added sb round

--- routes/helpers/imgUploadHelper.js
+++ routes/helpers/imgUploadHelper.js
@@ -1,4 +1,4 @@
 const getImgUploadURL = () => `http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/uploaded-img/`
-const getFileUploadURL = () => `http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/uploaded-file/`
+const getFileUploadURL = () => `http://localhost:4333/uploaded-file/`
 
 module.exports = { getImgUploadURL, getFileUploadURL };
