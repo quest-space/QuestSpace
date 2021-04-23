5394c72a023f4be97729bcb5b000c29e233242b0
Talha Waheed <talhawaheed57@gmail.com>
2021-04-23 22:44:06 +0500

    changed default auth to cater host too

--- routes/authentication/defaultAuthentication.js
+++ routes/authentication/defaultAuthentication.js
@@ -12,4 +12,16 @@ router.use((req, res, next) => {
   next();
 })
 
+router.use(`/:type`, (req, res, next) => {
+  if (req.params.type === `participant` || req.params.type === `host`) {
+    req.body.id = "6070f3b5535b23e65b3d0160";
+    req.body.username = req.params.type === `participant` ? "HassaanAW" : "Admin";
+    req.body.userType = req.params.type;
+    req.id = "6070f3b5535b23e65b3d0160";
+    req.username = req.params.type === `participant` ? "HassaanAW" : "Admin";
+    req.userType = req.params.type;
+  }
+  next();
+})
+
 module.exports = router;
