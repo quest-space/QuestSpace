17d9c193a39273ac66e25539f19dcc27c646e97e
Talal <talaltouseef@gmail.com>
2021-04-23 17:54:46 +0500

    Tabs changes with API call result

--- front-end/src/components/HostRound.js
+++ front-end/src/components/HostRound.js
@@ -9,22 +9,49 @@ import Tabs from "./Tabs"
 const HostRound = () => {
 
     const { roundID, questID } = useParams()
+    const history = useHistory()
 
     const [tab, setTab] = React.useState("Details");
+    const [roundFetched, setRoundFetched] = React.useState(false)
+    const [roundDetails, setRoundDetails] = React.useState({})
 
-    
+    const fetchRoundDetails = async () => {
+        const response = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/apitest/participant/quest/${questID}/${roundID}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+        })
 
+        const responseBody = await response.json()
 
+        if (response.status !== 200) {
+            console.log(`Error in fetching roundDetails.`)
+            alert(JSON.stringify(responseBody), "Returning back to quest page")
+            // history.replace(`/hosthomepage/quest/${questID}`)
+        } else {
+            responseBody["roundType"] = `Submission`
+            setRoundDetails(responseBody)
+        }
+
+    }
+
+    if (!roundFetched) {
+        setRoundFetched(true)
+        fetchRoundDetails()
+        console.log(`Round details fetched.`)
+    }
 
     return (
         <React.Fragment>
             <MainNavbar />
-            <Header />
-            {/* <BreadCrumb items={[{ text: "Home", to: "/participanthomepage" }, { text: roundDetails.questName, to: `/participanthomepage/quest/${questID}` }, { text: `Round ${roundID}`, to: `/participanthomepage/quest/${questID}/round/${roundID}` }]} /> */}
+            <Header heading={`Round ${roundID}: ${roundDetails.roundName}`} subheading={roundDetails.roundType} />
+            <BreadCrumb items={[{ text: "Home", to: "/participanthomepage" }, { text: roundDetails.questName, to: `/participanthomepage/quest/${questID}` }, { text: `Round ${roundID}`, to: `/participanthomepage/quest/${questID}/round/${roundID}` }]} />
 
-            {roundID} {questID} {tab}
+            {tab} {roundDetails.roundType}
 
-            <Tabs setTab={setTab} submission={true}/>
+            <Tabs setTab={setTab} roundType={roundDetails.roundType} />
 
             <PageFooter />
         </React.Fragment>
