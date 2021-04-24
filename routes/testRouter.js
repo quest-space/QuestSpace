37fc69148e1845b434fae6f75e9c5e264cd8c2d7
Talha Waheed <talhawaheed57@gmail.com>
2021-04-25 01:54:45 +0500

    added more details to who-am-i

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -1,6 +1,9 @@
 const { Router } = require (`express`);
 const router = Router();
 
+const Participant = require(`../models/participant`);
+const Host = require(`../models/host`);
+
 const { sendRes } = require(`./helpers/sendRes`);
 const BAQ_REQUEST_STATUS_CODE = 400;
 const OK_STATUS_CODE = 200;
@@ -20,20 +23,34 @@ router.use(`/auth`, require(`./authentication/signUpInAPIs`));
 // As an alternative, use this:
 router.use(require(`./authentication/defaultAuthentication`));
 
-router.post(`/who-am-i`, (req, res) => {
-    try {
-      sendRes(res, OK_STATUS_CODE, {
-        username: req.body.username,
-        type: req.body.userType
-      });
+router.post(`/who-am-i`, async (req, res) => {
+  try {
+    const returnObj = {
+      username: req.body.username,
+      type: req.body.userType
     }
-    catch(err) {
-      console.log("Error")
-      sendRes(res, BAQ_REQUEST_STATUS_CODE, {
-        error: err
-      });
+    if (req.body.userType === `participant`) {
+      const partDetails = await Participant.findOne({ username: req.body.username });
+      returnObj[`firstname`] = partDetails.firstname;
+      returnObj[`lastname`] = partDetails.lastname;
+      returnObj[`dateofbirth`] = partDetails.dateofbirth;
+      returnObj[`organization`] = partDetails.organization;
+    } else {
+      const hostDetails = await Host.findOne({ username: req.body.username });
+      returnObj[`organization`] = hostDetails.organization;
+      returnObj[`representativeName`] = hostDetails.representativeName;
+      returnObj[`representativeDesignation`] = hostDetails.representativeDesignation;
+      returnObj[`rating`] = hostDetails.rating;
     }
-  });
+    sendRes(res, OK_STATUS_CODE, returnObj);
+  }
+  catch(err) {
+    console.log("Error")
+    sendRes(res, BAQ_REQUEST_STATUS_CODE, {
+      error: err
+    });
+  }
+});
   
   
 
