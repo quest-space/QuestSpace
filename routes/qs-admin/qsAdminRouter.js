8c56f7c75929cf5828ee3bc26d1f0abd8e908c7f
Talha Waheed <talhawaheed57@gmail.com>
2021-04-25 18:13:10 +0500

    added accept/reject apis

--- routes/qs-admin/qsAdminRouter.js
+++ routes/qs-admin/qsAdminRouter.js
@@ -3,5 +3,7 @@ const router = Router();
 
 router.use(`/signout`, require(`../authentication/signOutAPI`));
 router.use(`/homepage`, require(`./qsAdminHomepage`));
+router.use(`/accept`, require(`./acceptQuestRequest`));
+router.use(`/reject`, require(`./rejectQuestRequest`));
 
 module.exports = router;
---
+++ routes/qs-admin/rejectQuestRequest.js
@@ -0,0 +1,54 @@
+const { Router } = require (`express`);
+const router = Router();
+
+const Host = require(`../../models/host`);
+const Quest = require(`../../models/quest`);
+const Participation = require(`../../models/participation`);
+const { sendRes } = require(`../helpers/sendRes`);
+const { getQuestStatus } = require(`../helpers/helperFunctions`);
+
+const UNPROCESSABLE_ENTRY_STATUS_CODE = 422;
+const BAQ_REQUEST_STATUS_CODE = 400;
+const CREATED_STATUS_CODE = 201;
+const OK_STATUS_CODE = 200;
+
+router.post(`/`, async (req, res) => {
+
+  try {
+
+    if (!req.body.questName) {
+      sendRes(res, BAQ_REQUEST_STATUS_CODE, {
+        error: "questName not provided"
+      });
+      return;
+    }
+    
+    const quest = await Quest.findOne({ questName: req.body.questName }).exec();
+
+    if (!quest) {
+      sendRes(res, BAQ_REQUEST_STATUS_CODE, {
+        error: "Quest does not exist"
+      });
+      return;
+    }
+
+    if (quest.status !== `pending`) {
+      sendRes(res, BAQ_REQUEST_STATUS_CODE, {
+        error: "You cannot reject an accepted/rejected quest"
+      });
+      return;
+    }
+
+    const updateQuest = updateOne({ questName: req.body.questName }, { status: `rejected` });
+
+    sendRes(res, OK_STATUS_CODE, updateQuest);
+
+  } catch (err) {
+    console.log(err);
+    sendRes(res, UNPROCESSABLE_ENTRY_STATUS_CODE, {
+      error: err.message
+    });
+  }
+})
+
+module.exports = router;
