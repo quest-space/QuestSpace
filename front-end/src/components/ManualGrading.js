6f1de2cb703cb9a3bb297bbccb76a8eca65dff69
Talal <talaltouseef@gmail.com>
2021-04-25 23:52:33 +0500

    Changed apitest to api

--- front-end/src/components/ManualGrading.js
+++ front-end/src/components/ManualGrading.js
@@ -21,7 +21,7 @@ const ManualGrading = (props) => {
             })
         })
 
-        const response = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/apitest/host/quest/${questID}/${roundID}/grade`, {
+        const response = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/api/host/quest/${questID}/${roundID}/grade`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
