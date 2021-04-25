8844f0326e528405261983f3395c86ae2ffc3c3f
Talal <talaltouseef@gmail.com>
2021-04-25 21:36:47 +0500

    Manual grading done

--- front-end/src/components/CardsContainerHost.js
+++ front-end/src/components/CardsContainerHost.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Cards from "./Cards"
-import {Link, useHistory} from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 
 /* Example call
 <CardsContainer tab="Home"/>
@@ -9,30 +9,30 @@ import {Link, useHistory} from "react-router-dom"
 // let cards = {}
 
 const headings = {
-    "live" : "Live Quests",
+    "live": "Live Quests",
     "upcoming": "Upcoming Quests",
-    "past" : "Past Quests",
-    "all" : "All Quests",
-    "pending" : "Pending Quests",
+    "past": "Past Quests",
+    "all": "All Quests",
+    "pending": "Pending Quests",
 }
 
 const CardsContainer = (props) => {
 
     const history = useHistory()
-    const [response, setResponse] = React.useState({"all":{}})
+    const [response, setResponse] = React.useState({ "all": {} })
     const [render, setRender] = React.useState(false);
 
     const showError = (errors) => {
         alert(JSON.stringify(errors))
     }
-   
+
     const apiCall = async () => {
-        const resp = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/apitest/host/homepage`,{
-            method:"POST",
-            headers:{
+        const resp = await fetch(`http://ec2-13-233-137-233.ap-south-1.compute.amazonaws.com/apitest/host/homepage`, {
+            method: "POST",
+            headers: {
                 'Content-Type': 'application/json'
             },
-            credentials:"include",
+            credentials: "include",
             body: JSON.stringify({
                 username: "HassaanAW",
                 password: "hassaan123",
@@ -52,81 +52,84 @@ const CardsContainer = (props) => {
     }
 
     let cards = response
-    if(!render){
+    if (!render) {
         setRender(true)
         apiCall()
     }
 
     const toCreate = () => {
-        history.push({pathname: "/hosthomepage/createquest"})
+        history.push({ pathname: "/hosthomepage/createquest" })
     }
 
-    return(
+    return (
         <div>
-                <button class="blueButton1" onClick={()=>{toCreate()}}>New Quest<span className="material-icons" style={{ fontSize: "20pt", color: "#FFFFFF", float:"right" }}>add</span></button>
-                <div className="container" id="cc" style={{marginTop:'0rem', paddingBottom:"5.5rem"}}>
-                    <div style={{
-                        paddingLeft:'1.5rem', 
-                        paddingTop:'5.5rem', 
-                        fontStyle: 'normal',
-                        fontWeight: 'normal',
-                        fontSize: '32px'}}>
-                        {headings[props.tab]}
-                    </div>
-                 
-                    {cards[props.tab].length > 0 &&
+            <button className="blueButton1" onClick={() => { toCreate() }}>New Quest<span className="material-icons" style={{ fontSize: "20pt", color: "#FFFFFF", float: "right" }}>add</span></button>
+            <div className="container" id="cc" style={{ marginTop: '0rem', paddingBottom: "5.5rem" }}>
+                <div style={{
+                    paddingLeft: '1.5rem',
+                    paddingTop: '5.5rem',
+                    fontStyle: 'normal',
+                    fontWeight: 'normal',
+                    fontSize: '32px'
+                }}>
+                    {headings[props.tab]}
+                </div>
+
+                {cards[props.tab].length > 0 &&
                     <div>
-                    {
-                    Object.keys(cards[props.tab]).map((cardList, j)=>{
-                        return(
-                        <div key={j} className="row" style={{padding:'1.5rem'}}>
-                        {Object.keys(cards[props.tab][cardList]).map((card, k)=>{
-                            return(
-                                <div key={k} className="col-lg-3 col-md-6 mb-4 mb-lg-0">
-                                    {/* {console.log(cards[key][cardList][card].hostUser)} */}
-                                    {
-                                    props.tab !== "pending" ? 
-                                    <Link to={{pathname: "hosthomepage/quest/"+cards[props.tab][cardList][card].questID}}><Cards 
-                                        imgUrl = {cards[props.tab][cardList][card].logoURL}
-                                        title = {cards[props.tab][cardList][card].questName}
-                                        host = {cards[props.tab][cardList][card].hostUser}
-                                        description = {cards[props.tab][cardList][card].description}  
-                                        date = {cards[props.tab][cardList][card].startDate}
-                                        starsCount = {cards[props.tab][cardList][card].rating}
-                                    />
-                                    </Link>
-                                    :
-                                    <Cards 
-                                        imgUrl = {cards[props.tab][cardList][card].logoURL}
-                                        title = {cards[props.tab][cardList][card].questName}
-                                        host = {cards[props.tab][cardList][card].hostUser}
-                                        description = {cards[props.tab][cardList][card].description}  
-                                        date = {cards[props.tab][cardList][card].startDate}
-                                        starsCount = {cards[props.tab][cardList][card].rating}
-                                    />
-                                    }
-                                </div>       
-                            )
-                        })}
-                        
-                        </div>
-                    
-                    )})
-                    }
+                        {
+                            Object.keys(cards[props.tab]).map((cardList, j) => {
+                                return (
+                                    <div key={j} className="row" style={{ padding: '1.5rem' }}>
+                                        {Object.keys(cards[props.tab][cardList]).map((card, k) => {
+                                            return (
+                                                <div key={k} className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                                                    {/* {console.log(cards[key][cardList][card].hostUser)} */}
+                                                    {
+                                                        props.tab !== "pending" ?
+                                                            <Link to={{ pathname: "hosthomepage/quest/" + cards[props.tab][cardList][card].questID }}><Cards
+                                                                imgUrl={cards[props.tab][cardList][card].logoURL}
+                                                                title={cards[props.tab][cardList][card].questName}
+                                                                host={cards[props.tab][cardList][card].hostUser}
+                                                                description={cards[props.tab][cardList][card].description}
+                                                                date={cards[props.tab][cardList][card].startDate}
+                                                                starsCount={cards[props.tab][cardList][card].rating}
+                                                            />
+                                                            </Link>
+                                                            :
+                                                            <Cards
+                                                                imgUrl={cards[props.tab][cardList][card].logoURL}
+                                                                title={cards[props.tab][cardList][card].questName}
+                                                                host={cards[props.tab][cardList][card].hostUser}
+                                                                description={cards[props.tab][cardList][card].description}
+                                                                date={cards[props.tab][cardList][card].startDate}
+                                                                starsCount={cards[props.tab][cardList][card].rating}
+                                                            />
+                                                    }
+                                                </div>
+                                            )
+                                        })}
+
+                                    </div>
+
+                                )
+                            })
+                        }
                     </div>}
 
-                    {cards[props.tab].length === 0 && 
-                        <div style={{
-                            border: "1px solid #C4C4C4", 
-                            boxShadow: "1px 2px 10px 2px rgba(0, 0, 0, 0.1)", 
-                            // marginBottom:"5.5rem",
-                            margin:"1.5rem",
-                            padding:"2rem"}}>
-                                <i class="fas fa-exclamation-circle"></i> Not Available
-                            </div>
-                    }
+                {cards[props.tab].length === 0 &&
+                    <div style={{
+                        border: "1px solid #C4C4C4",
+                        boxShadow: "1px 2px 10px 2px rgba(0, 0, 0, 0.1)",
+                        // marginBottom:"5.5rem",
+                        margin: "1.5rem",
+                        padding: "2rem"
+                    }}>
+                        <i className="fas fa-exclamation-circle"></i> Not Available
+                    </div>
+                }
 
-                </div>
+            </div>
 
         </div>
 
